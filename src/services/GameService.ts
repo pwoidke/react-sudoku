c4fe0e58205913c479e6eaa5072ea4af601904d0
PaulWoidkeGoodyear <90349595+PaulWoidkeGoodyear@users.noreply.github.com>
2021-11-30 21:07:17 -0500

    Use Axios for API calls instead of fetch

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -2,9 +2,12 @@ import { Game } from '../types/index';
 
 import { environment } from '../environment';
 
+import axios from 'axios';
+
 export async function getNewGame(difficulty: string): Promise<Game> {
-  const response = await fetch(
+  const response = await axios.get(
     `${environment.apiOptions.baseUrl}${environment.apiOptions.endpoints.generate}?difficulty=${difficulty}`
   );
-  return await response.json();
+  console.log('response  ', response);
+  return response.data;
 }
