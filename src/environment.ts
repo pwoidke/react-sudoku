00abe6e14c39ca75c041afe0b51160d18c9d30ae
Paul W <pwoidke@gmail.com>
2023-02-05 21:43:59 -0500

    Update API endpoint

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   apiOptions: {
-    baseUrl: 'https://vast-chamber-17969.herokuapp.com',
+    baseUrl: 'https://sudoku-generator1.p.rapidapi.com', // https://rapidapi.com/gregor-i/api/sudoku-generator1
+    path: '/sudoku',
     retryCalls: '3',
     retryDelay: '5000',
     endpoints: {
--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -1,10 +1,19 @@
-import { Game } from '../types';
+import { Difficulty, Game } from '../types';
 
 import { environment } from '../environment';
 
-export async function getNewGame(difficulty: string): Promise<Game> {
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3ea5b334eamshb48e51ac85d7f06p13eaf4jsnf9610d474885',
+		'X-RapidAPI-Host': 'sudoku-generator1.p.rapidapi.com'
+	}
+};
+
+export async function getNewGame(difficulty: Difficulty): Promise<Game> {
   const response = await fetch(
-    `${environment.apiOptions.baseUrl}${environment.apiOptions.endpoints.generate}?difficulty=${difficulty}`
+    `${environment.apiOptions.baseUrl}${environment.apiOptions.path}${environment.apiOptions.endpoints.generate}?difficulty=${difficulty}`,
+    options
   );
   return await response.json();
 }
